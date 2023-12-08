70cc78198412b03aaa1f42f9ea8688ea0b0f96d8
oswin <oswinalex1@gmail.com>
2023-12-09 01:17:40 +0530

    ui

--- app/src/App.js
+++ app/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { BrowserRouter as Router ,Route, Routes} from "react-router-dom"
 import User_register from './component/user_register';
 import Login from './component/login';
+import Main from './component/main';
 function App() {
   return (
     <>
           <Router>
         <Routes>
+          <Route path="/" Component={Main}/>
           <Route path="/User_register" Component={User_register}/>
           
           <Route path="/Login" Component={Login}/>
---
+++ app/src/component/main.js
@@ -0,0 +1,12 @@
+import "./main.css"
+
+let Main =()=>{
+    return(
+        <>
+        <div>
+            
+        </div>
+        </>
+    )
+}
+export default Main
